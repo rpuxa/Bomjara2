217554b941f0536847e6d1679cdc694ec7a15b55
Grigorii Yurkov <gr0070@mail.ru>
2018-10-03 11:44:24 +0400

    ScrollButtonView finished

--- app/src/main/java/ru/rpuxa/bomjara2/ScrollButtonsView.kt
+++ app/src/main/java/ru/rpuxa/bomjara2/ScrollButtonsView.kt
@@ -6,21 +6,49 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.support.v4.view.ViewPager
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
+import kotlin.math.*
 
 class ScrollButtonsView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
     private val frameWidth = 5f
+    private val cursorSpeed = 10f
 
+    private var iconWidth = width - 2 * frameWidth
     private val paint = Paint()
     private var cursorIcon: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.frame)
-    private lateinit var icons: Array<Bitmap>
-    private var cursor = 0f
+    private var icons: Array<Bitmap> = emptyArray()
+    private var drugging = false
+    private var cursorStartDrugging = 0f
+    private var _cursor = 0f
+    fun setCursor(value: Float, fromPager: Boolean) {
+        _cursor = value
+        if (!fromPager) {
+            val pager = controlViewPager
+            if (pager != null) {
+                pager.beginFakeDrag()
+                val current = (cursorStartDrugging - value) * pager.width / (iconWidth + frameWidth)
+                pager.fakeDragBy(current)
+                cursorStartDrugging = value
+            }
+        } else {
+            cursorTarget = value
+        }
+    }
+
+    private var cursorTarget = 0f
+
+    private var controlViewPager: ViewPager? = null
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        iconWidth = w - 2 * frameWidth
+    }
 
     @SuppressLint("DrawAllocation")
     override fun onDraw(canvas: Canvas) {
-        val iconWidth = width - 2 * frameWidth
         val iconWidthInt = iconWidth.toInt()
         val withFrame = iconWidthInt + frameWidth.toInt() * 2
 
@@ -34,12 +62,73 @@ class ScrollButtonsView(context: Context, attrs: AttributeSet) : View(context, a
         }
         if (cursorIcon.width != withFrame || cursorIcon.height != withFrame)
             cursorIcon = Bitmap.createScaledBitmap(cursorIcon, withFrame, withFrame, false)
-        canvas.drawBitmap(cursorIcon, 0f, cursor, paint)
+        canvas.drawBitmap(cursorIcon, 0f, _cursor, paint)
+
+        if (_cursor != cursorTarget) {
+            val delta = cursorTarget - _cursor
+            val pos = _cursor + sign(delta) * max(abs(delta) * cursorSpeed / iconWidth, cursorSpeed / 4)
+            setCursor(if (delta > 0) min(cursorTarget, pos) else max(cursorTarget, pos), false)
+            invalidate()
+        } else if (!drugging) {
+            controlViewPager.endDrag()
+        }
     }
 
-    fun setIcons(vararg icons: Bitmap): ScrollButtonsView {
-        this.icons = Array(icons.size) { icons[it] }
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val iconWithFrame = iconWidth + frameWidth
+        var touchY = event.y - iconWidth / 2
+        if (touchY < 0)
+            touchY = 0f
+        val maxY = iconWithFrame * (icons.size - 1)
+        if (touchY > maxY)
+            touchY = maxY
+
+        cursorTarget = when (event.action) {
+            MotionEvent.ACTION_UP -> {
+                drugging = false
+                round(touchY / iconWithFrame) * iconWithFrame
+            }
+            else -> {
+                drugging = true
+                touchY
+            }
+        }
+        if (cursorTarget != _cursor)
+            invalidate()
+        return true
+    }
+
+    fun setIcons(vararg icons: Int): ScrollButtonsView {
+        this.icons = Array(icons.size) { BitmapFactory.decodeResource(resources, icons[it])!! }
         invalidate()
         return this
+    }
+
+    fun setViewPager(viewPager: ViewPager) {
+        controlViewPager = viewPager
+        if (viewPager.adapter.count != icons.size)
+            throw IllegalStateException("sizes not same!!")
+        invalidate()
+        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                if (!viewPager.isFakeDragging) {
+                    val full = iconWidth + frameWidth
+                    setCursor((position + positionOffset) * full, true)
+                    invalidate()
+                }
+            }
+
+            override fun onPageSelected(position: Int) {
+            }
+        })
+    }
+
+    private fun ViewPager?.endDrag() {
+        if (this != null && isFakeDragging)
+            endFakeDrag()
+        cursorStartDrugging = _cursor
     }
 }
--- app/src/main/java/ru/rpuxa/bomjara2/activities/MenuActivity.kt
+++ app/src/main/java/ru/rpuxa/bomjara2/activities/MenuActivity.kt
@@ -1,18 +1,48 @@
 package ru.rpuxa.bomjara2.activities
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentPagerAdapter
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.menu.*
-import ru.rpuxa.bomjara2.MenuFragmentAdapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.content.*
 import ru.rpuxa.bomjara2.R
+import ru.rpuxa.bomjara2.R.drawable
 
 class MenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.menu)
+        setContentView(R.layout.content)
+        scroll_buttons.setIcons(drawable.info, drawable.friend)
+        pager.adapter = object : FragmentPagerAdapter(supportFragmentManager) {
+            val ar = arrayOf(
+                    H1(), H2()
 
-        //pager.adapter = MenuFragmentAdapter(supportFragmentManager).openSaves()
+            )
 
+            override fun getItem(position: Int): Fragment {
+                return ar[position]
+            }
+
+            override fun getCount() = ar.size
+        }
+
+        scroll_buttons.setViewPager(pager)
     }
+
+    class H1 : Fragment() {
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+            return inflater.inflate(R.layout.h1, container, false)
+        }
+    }
+
+    class H2 : Fragment() {
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+            return inflater.inflate(R.layout.h2, container, false)
+        }
+    }
+
 }
